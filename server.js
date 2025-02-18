fe7aa55d39a8ceda52b4bef0d9813676e173607d
maleque <benhassinemaleque8@gmail.com>
2025-02-18 11:04:33 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config()
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+console.log("connected to database");
+}).catch(err=>console.log(err))
+
+app.listen(process.env.PORT, () => {
+  console.log("listen on PORT" + PORT);
+});
